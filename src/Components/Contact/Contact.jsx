4c385014770294e8c9ba2241f3cea7af2aa77d78
Theriomich <theriomich@gmail.com>
2023-07-05 18:35:14 -0400

    agregue formulario con correo

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,11 +1,44 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import "./Contact.css";
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "Contact_Mich",
+        "template_ugsje2l",
+        form.current,
+        "CGJJmH1sYuCRwf5T9"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <>
       <h1 className="TitleContact">Contacto</h1>;
-      <p>Correo: M.cifuentes.mankoch@gmail.com</p>
-      <p>Número de teléfono: 984943280</p>
+      <form ref={form} onSubmit={sendEmail} className="field">
+        <label>Nombre</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Mensaje</label>
+        <textarea name="message" style={{ width: "250px" }} />
+        <input type="submit" value="Enviar" />
+      </form>
+      <h3 className="subtitleContact">Contactame directamente por:</h3>
+      <p className="paragraph">Correo: M.cifuentes.mankoch@gmail.com</p>
+      <p className="paragraph">Número de teléfono: 984943280</p>
     </>
   );
 }
